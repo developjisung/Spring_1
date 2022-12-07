77ab958d02cf902b4236a18723dae97bb0ad013b
developjisung <developjisung@gmail.com>
2022-12-07 17:17:40 +0900

    Spring_level1_project 연관관계 매핑 수정

--- src/main/java/com/sparta/hanghaememo/entity/Memo.java
+++ src/main/java/com/sparta/hanghaememo/entity/Memo.java
@@ -49,6 +49,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -72,6 +74,9 @@ public class Memo extends Timestamped {
     
     @Column(nullable = false)       // 작성자 ID
     private Long userid;
+
+    @OneToMany(mappedBy = "memo", cascade = CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<>();
 
 
     // Dto -> Entity
